8cdf4bff62c322f5f35e5676879db68011938085
Andrew <andcliu@gmail.com>
2020-07-04 23:18:49 -0700

    router added

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-	<div>
-		Home
+	<div id='homeBg'>
 	</div>
 </template>
+
+<script>
+	import 'images/dining_area.png'
+</script>
+
+<style scoped>
+	#homeBg{
+		background-image:url('./images/dining_area.png');
+		background-size: cover;
+		background-position: center;
+		height:100vh;
+	}
+</style>
